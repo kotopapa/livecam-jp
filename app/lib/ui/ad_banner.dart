a2ee0687476d185bbddc695c165f934c9125e916
kotopapa <t.fujiwara.pvt.0111@gmail.com>
2026-08-30 17:40:13 +0900

    ad_banner: google_mobile_ads の AppState と名前衝突を回避

--- app/lib/ui/ad_banner.dart
+++ app/lib/ui/ad_banner.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart' hide AppState;
 
 import '../app_state.dart';
 import '../config.dart';
